ff24aa5e8201c86d91a3979ff2ef9d528b3bc1f2
yixianLu <43053645+yixianLu@users.noreply.github.com>
2021-03-06 05:50:26 +0800

    Update AuthorCard.jsx

--- src/components/authors/AuthorCard.jsx
+++ src/components/authors/AuthorCard.jsx
@@ -1,9 +1,29 @@
 import React, { Component } from 'react'
 import { Button } from "@material-ui/core"
 import axios from 'axios';
+import { connect } from 'react-redux';
 
 class AuthorCard extends Component {
   follow = async () => {
+    var login_author_id = this.props.authorID.authorID
+    var want_to_add_id = this.props.author.authorID
+    var post_information = {
+      "type": "follow",
+      "new_follower_ID": ""
+    }
+    post_information['new_follower_ID'] = login_author_id
+    if (login_author_id === want_to_add_id){
+      window.alert('you are adding yourself')
+    }else{
+      try {
+        let doc = await axios.post(`service/author/${want_to_add_id}/inbox/`, post_information)
+        if (doc.status === 200) {
+          console.log(doc)
+        }
+      } catch (err) {
+        console.log(err.response.status)
+      }
+    }
 
   }
 
@@ -16,5 +36,8 @@ class AuthorCard extends Component {
     )
   }
 }
+const mapStateToProps = (state) => ({
+  authorID: state.user.authorID
+})
 
-export default AuthorCard;
+export default connect(mapStateToProps)(AuthorCard);
