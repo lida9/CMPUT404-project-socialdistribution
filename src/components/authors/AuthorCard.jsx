553610784f9b74e2d7112c3875ddde535cbe9b6b
hz6 <hz6@ualberta.ca>
2021-03-05 20:34:17 +0800

    markdown text and  public friend selection

---
+++ src/components/authors/AuthorCard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AuthorCard extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default AuthorCard;
