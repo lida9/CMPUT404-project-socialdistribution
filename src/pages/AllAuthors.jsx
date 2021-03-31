f0121500eeea111a1694da710ccbe5724260c10a
hz6 <hz6@ualberta.ca>
2021-03-31 15:43:09 +0800

    remote authorId

--- src/pages/AllAuthors.jsx
+++ src/pages/AllAuthors.jsx
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from "axios";
 import AuthorCard from '../components/authors/AuthorCard';
+import { Button, TextField } from '@material-ui/core';
 
 class AllAuthors extends Component {
   constructor(props) {
     super(props);
     this.state = {
       currentUser: null,
-      authors: []
+      authors: [],
+      remoteAuthorId: "",
     }
   }
-
 
   componentDidMount = async () => {
     const { authorID } = this.props;
@@ -26,11 +27,55 @@ class AllAuthors extends Component {
     }
   }
 
+  sendRemote = async () => {
+    const { remoteAuthorId } = this.state;
+    const { authorID } = this.props;
+    if (authorID) {
+      if (remoteAuthorId) {
+        console.log("remoteAuthorId: ", remoteAuthorId);
+        let req_body = {
+          "type": "follow",
+          "new_follower_ID": authorID.authorID
+        }
+
+        try {
+          const doc = await axios.post(`/service/author/${remoteAuthorId}/inbox/`, req_body);
+          if (doc.status === 200) {
+            console.log("Success:", doc);
+          }
+        } catch (err) {
+          console.log(err.message);
+        }
+
+      } else {
+        alert("Cannot be empty!");
+      }
+    } else {
+      console.log("Not Logged in");
+    }
+  }
+
   render() {
-    const { currentUser, authors } = this.state;
+    const { currentUser, authors, remoteAuthorId } = this.state;
     console.log(authors);
     return (
       <div>
+        <TextField
+          style={{ width: "60%", margin: 10 }}
+          id="remote-author"
+          variant="outlined"
+          label="Friend AuthorID"
+          value={remoteAuthorId}
+          onChange={(e) => this.setState({ remoteAuthorId: e.target.value })}
+        />
+        <Button
+          style={{ marginTop: 10 }}
+          color="primary"
+          variant="outlined"
+          onClick={this.sendRemote}
+        >
+          Send Request
+        </Button>
         <div>
           {
             authors.length !== 0 ?
