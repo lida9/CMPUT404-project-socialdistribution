58379c45016b6778fc96e1332390a3d00dd36be8
Lida Ling <lida1@ualberta.ca>
2021-03-31 16:33:26 -0600

    change inbox

--- socialdistribution/api_views/inbox_view.py
+++ socialdistribution/api_views/inbox_view.py
@@ -124,24 +124,32 @@ def inbox_detail(request, authorID):
                     return Response({'message':'sent successfully!'}, status=status.HTTP_200_OK)
                 except Author.DoesNotExist:
                     get_author_url = 'https://citrusnetwork.herokuapp.com/service/author/' + authorID
-                    r_author = requests.get(get_author_url, data=json.dumps(data), auth=('CitrusNetwork','oranges'))
+                    r_author = requests.get(get_author_url, auth=('CitrusNetwork','oranges'))
                     if r_author.status_code == 200:
-                        author_information = r_author.content
-                        author_information = author_information.decode("utf-8")
-                        data['author'] = author_information
-                        data['type'] = 'Like'
-                        author_like_ID = data['author_like_ID']
-                        author_Like = Author.objects.get(authorID = author_like_ID)
+                        # author_information = r_author.content
+                        # author_information = author_information.decode("utf-8")
+                        # data['author'] = author_information
+                        # data['type'] = 'Like'
+                        # author_like_ID = data['author_like_ID']
+                        # author_Like = Author.objects.get(authorID = author_like_ID)
                         postID = data["postID"]
-                        data['summary'] = author_Like.username + " Likes your post"
-                        data['@context'] = "https://www.citrusnetwork.herokuapp.com/service/author/" + authorID + "/view-post/" + postID + "/"
-                        data['object'] = "http://127.0.0.1:5454/author/"+ authorID +"/posts/"+postID
+                        # data['summary'] = author_Like.username + " Likes your post"
+                        # data['@context'] = "https://www.citrusnetwork.herokuapp.com/service/author/" + authorID + "/view-post/" + postID + "/"
+                        # data['object'] = "http://127.0.0.1:5454/author/"+ authorID +"/posts/"+postID
 
-                        del data['author_like_ID']
-                        del data['author_write_article_ID']
-                        del data['postID']
+                        # del data['author_like_ID']
+                        # del data['author_write_article_ID']
+                        # del data['postID']
+                        new_data = {"type":"Like"}
+                        author_who_liked = Author.objects.get(authorID = data['author_like_ID'])
+                        serializer = AuthorSerializer(author_who_liked)
+                        new_data['author'] = serializer.data
+                        new_data['summary'] = author_who_liked.username + " Likes your post"
+                        new_data['@context'] = "https://www.citrusnetwork.herokuapp.com/service/author/" + authorID + "/view-post/" + postID + "/"
+                        new_data['object'] = "https://www.citrusnetwork.herokuapp.com/service/author/"+ authorID +"/posts/"+postID
+
                         inbox_url = 'https://citrusnetwork.herokuapp.com/service/author/' + authorID + '/inbox/'
-                        response = requests.post(inbox_url, data=json.dumps(data), auth=('CitrusNetwork','oranges'))
+                        response = requests.post(inbox_url, data=json.dumps(new_data), auth=('CitrusNetwork','oranges'))
                         if response.status_code == 201:
                             return Response({'message':'sent successfully!'}, status=status.HTTP_200_OK)
                         else:
